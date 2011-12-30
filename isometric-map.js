7c6c28a4967e1510f9c0332219e39bcca0237996
Jess Telford <jess+git@jes.st>
2011-12-30 19:48:14 +1100

    Fix whitespace issues

--- isometric-map.js
+++ isometric-map.js
@@ -1,67 +1,67 @@
 ig.module(
-  'plugins.isometricmap.isometric-map'
+    'plugins.isometricmap.isometric-map'
 )
 .requires(
-  'impact.impact',
-  'impact.background-map'
+    'impact.impact',
+    'impact.background-map'
 )
 .defines(function(){
 
-  ig.IsometricMap = ig.BackgroundMap.extend({
+ig.IsometricMap = ig.BackgroundMap.extend({
 
-      tileOffsetX: 0,
-      tileOffsetY: 0,
-      tileOffsetZ: 0,
+    tileOffsetX: 0,
+    tileOffsetY: 0,
+    tileOffsetZ: 0,
 
-      pxOffsetX: 0,
-      pxOffsetY: 0,
+    pxOffsetX: 0,
+    pxOffsetY: 0,
 
-      pxMinX: 0,
-      pxMinY: 0,
-      pxMaxX: 0,
-      pxMaxY: 0,
+    pxMinX: 0,
+    pxMinY: 0,
+    pxMaxX: 0,
+    pxMaxY: 0,
 
-      originX: 0,
-      originY: 0,
-      originZ: 0,
+    originX: 0,
+    originY: 0,
+    originZ: 0,
 
     init: function( tilesize, data, tileset ) {
 
-      this.parent(tilesize, data, tileset);
+        this.parent(tilesize, data, tileset);
 
-      this.alpha = Math.atan(0.5);
-      this.recalculateAngles();
+        this.alpha = Math.atan(0.5);
+        this.recalculateAngles();
 
-      /*
-       This is calculated by determining the 'length' of one of the sides of
-       an isometric tile.
-       Derivation:
+        /*
+           This is calculated by determining the 'length' of one of the sides of
+           an isometric tile.
+           Derivation:
 
-       width = tilesize
-       height = (width + 2) / 2
+           width = tilesize
+           height = (width + 2) / 2
 
-       Given the tile:
-                           [(width / 2) + 1), 0]
-                          #*
-       [0, height / 2]  ##  ##
-                      *#      ##
-                      ##      ##
-                        ##  ##
-                          ##
+           Given the tile:
+           [(width / 2) + 1), 0]
+                              #*
+           [0, height / 2]  ##  ##
+                          *#      ##
+                          ##      ##
+                            ##  ##
+                              ##
 
-       The distance between the two points marked (*) can be written as:
+            The distance between the two points marked (*) can be written as:
 
-       length = sqrt( ((width / 2) + 1)^2 + (height / 2)^2 )
-       
-       The derivation is left as an exercise ;)
+            length = sqrt( ((width / 2) + 1)^2 + (height / 2)^2 )
 
-       */
-      this.edgeLength = ((this.tilesize + 2) / 2) * Math.sqrt(5) / 2;
+            The derivation is left as an exercise ;)
+
+        */
+        this.edgeLength = ((this.tilesize + 2) / 2) * Math.sqrt(5) / 2;
     },
 
     recalculateAngles: function() {
-      this.sinAlpha = Math.sin(this.alpha);
-      this.cosAlpha = Math.cos(this.alpha);
+        this.sinAlpha = Math.sin(this.alpha);
+        this.cosAlpha = Math.cos(this.alpha);
     },
 
     setScreenPos: function(x, y) {
@@ -82,7 +82,7 @@ ig.module(
         this.tileOffsetZ = (this.scroll.y / this.tilesize).toInt();
 
         // world origin (in screen coordinates) based on focus tile
-        this.originX = (this.tileOffsetX - this.tileOffsetZ) * this.cosAlpha * this.edgeLength,
+        this.originX = (this.tileOffsetX - this.tileOffsetZ) * this.cosAlpha * this.edgeLength;
         this.originY = ((this.tileOffsetX + this.tileOffsetZ + 1) * this.sinAlpha - this.tileOffsetY) * this.edgeLength;
 
         this.originX += this.pxOffsetX;
@@ -92,70 +92,71 @@ ig.module(
 
     draw: function() {
 
-      if(!this.tiles.loaded) {
-        return;
-      }
-      
-      this.drawTiled();
+        if(!this.tiles.loaded) {
+            return;
+        }
+
+        this.drawTiled();
     },
 
     drawTiled: function() {
 
-      var tile = 0,
-      anim = null,
-      iso_pxX = 0,
-      iso_pxY = 0;
+        var tile = 0,
+        anim = null,
+        iso_pxX = 0,
+        iso_pxY = 0;
 
-      
-      for( var mapY = -1, pxY = this.pxMinY; pxY < this.pxMaxY; mapY++, pxY += this.tilesize) {
-        var tileY = mapY + this.tileOffsetZ;
-      
-        // Repeat Y?
-        if( tileY >= this.height || tileY < 0 ) {
+        for (var mapY = -1, pxY = this.pxMinY; pxY < this.pxMaxY; mapY++, pxY += this.tilesize) {
 
-          if( !this.repeat ) {
-              continue;
-          }
+            var tileY = mapY + this.tileOffsetZ;
 
-          tileY = tileY > 0
-            ? tileY % this.height
-            : ((tileY+1) % this.height) + this.height - 1;
-        }
-      
-        for( var mapX = -1, pxX = this.pxMinX; pxX < this.pxMaxX; mapX++, pxX += this.tilesize ) {
-          var tileX = mapX + this.tileOffsetX;
-      
-          // Repeat X?
-          if( tileX >= this.width || tileX < 0 ) {
+            // Repeat Y?
+            if( tileY >= this.height || tileY < 0 ) {
 
-            if( !this.repeat ) {
-                continue;
+                if( !this.repeat ) {
+                    continue;
+                }
+
+                tileY = tileY > 0
+                    ? tileY % this.height
+                    : ((tileY+1) % this.height) + this.height - 1;
             }
 
-            tileX = tileX > 0
-              ? tileX % this.width
-              : ((tileX+1) % this.width) + this.width - 1;
-          }
+            for (var mapX = -1, pxX = this.pxMinX; pxX < this.pxMaxX; mapX++, pxX += this.tilesize) {
 
-          // Draw!
-          if( (tile = this.data[tileY][tileX]) ) {
+                var tileX = mapX + this.tileOffsetX;
 
-              // tile to be rendered
-              var xw = tileX;
-              var yw = 0;
-              var zw = tileY;
+                // Repeat X?
+                if( tileX >= this.width || tileX < 0 ) {
 
-              // screen coordinates of arbitrary world tile
-              var xs = ((xw - zw) * this.cosAlpha * this.edgeLength) - this.originX;
-              var ys = (((xw + zw) * this.sinAlpha - yw) * this.edgeLength) - this.originY;
+                    if( !this.repeat ) {
+                        continue;
+                    }
 
-              this.tiles.drawTile(xs.round(), ys.round(), tile-1, this.tilesize );
-          }
+                    tileX = tileX > 0
+                        ? tileX % this.width
+                        : ((tileX+1) % this.width) + this.width - 1;
+                }
 
+                // Draw!
+                if( (tile = this.data[tileY][tileX]) ) {
+
+                    // tile to be rendered
+                    var xw = tileX;
+                    var yw = 0;
+                    var zw = tileY;
+
+                    // screen coordinates of arbitrary world tile
+                    var xs = ((xw - zw) * this.cosAlpha * this.edgeLength) - this.originX;
+                    var ys = (((xw + zw) * this.sinAlpha - yw) * this.edgeLength) - this.originY;
+
+                    this.tiles.drawTile(xs.round(), ys.round(), tile-1, this.tilesize );
+                }
+
+            }
         }
-      }
     }
 
-  });
+});
 
 });
